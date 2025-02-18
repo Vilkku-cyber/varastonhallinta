8ec57c72e8304d935ae6cebf5bdd65ae47f05f9a
Vilkku-cyber <vilhelm.lehtomaki@gmail.com>
2025-02-18 12:39:57 +0200

    Test logging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    host: true,
-    port: 5173 // Voit vaihtaa portin tarvittaessa
+  build: {
+    rollupOptions: {
+      external: ['react-datepicker']
+    }
   }
 });
