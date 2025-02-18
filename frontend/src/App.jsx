f37f76e34db23a9842bd57ecfacb51d31d102532
Vilkku-cyber <vilhelm.lehtomaki@gmail.com>
2025-02-18 14:17:17 +0200

    Updated package-lock.json and fixed App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Inventory from "./Inventory";
+import AddProduct from "./AddProduct";
+import ProductDetails from "./ProductDetails";
+import EditTrip from "./EditTrip";
+import PastTrips from "./PastTrips";
+import CreateTrip from "./CreateTrip";
+import { database } from "./firebaseConfig";  // Firebase-yhteys
+import TestDatePicker from "./TestDatePicker";
 
 function App() {
   return (
-    <div>
-      <h1>Varastonhallinta</h1>
-      <p>Tervetuloa varastonhallintajärjestelmään.</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<TestDatePicker />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/edit-trip/:id" element={<EditTrip />} />
+        <Route path="/past-trips" element={<PastTrips />} />
+        <Route path="/create-trip" element={<CreateTrip />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App; // ✅ Tämä rivi on pakollinen, jotta `import App from "./App";` toimii
+export default App;
