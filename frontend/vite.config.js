3cafdbb8fe74535c31917e73ab9a52b5701f8a1d
Vilkku-cyber <vilhelm.lehtomaki@gmail.com>
2025-02-18 13:08:17 +0200

    Fix Vite environment variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  define: {
+    'process.env': process.env, // Tämä varmistaa, että ympäristömuuttujat toimivat Vite-sovelluksessa
+  },
   build: {
     rollupOptions: {
-      external: ['react-datepicker']
-    }
-  }
+      external: ['react-datepicker'],
+    },
+  },
 });
