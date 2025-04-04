ed37ea6970d2da9b5385ac030ef1f3fd9b2a2e2f
Vilkku-cyber <vilhelm.lehtomaki@gmail.com>
2025-04-04 11:55:00 +0300

    buttons fix

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -81,7 +81,7 @@ function Home() {
         <button className={styles.button} onClick={() => navigate("/pakkaus")}>Pakkaus</button>
         <button className={styles.button} onClick={() => navigate("/shelf-admin")}>Go to Shelf Admin</button>
         <button className={styles.button} onClick={() => navigate("/haku")}>hyllyhaku</button>
-        <button onClick={() => navigate('/qr-reader')}>Scan QR Code</button>
+        <button className={styles.button} onClick={() => navigate('/qr-reader')}>Scan QR Code</button>
         
 
       </div>
